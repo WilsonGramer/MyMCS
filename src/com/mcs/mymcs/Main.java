92271c1981ddc884522956687032f9278e3b7eb8
Wilsonator5000 <wilsonator5000@gmail.com>
2015-05-31 16:32:28 -0400

    Added com.mcs.mymcs.Main.java.

---
+++ src/com/mcs/mymcs/Main.java
@@ -0,0 +1,16 @@
+package com.mcs.mymcs;
+
+/**
+ * The Main class.
+ * @author Wilsonator5000
+ */
+public class Main {
+
+    public Main() {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+}
